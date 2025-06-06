b746dd0bfb6c497968e800964310084f0060cdd3
Gabriel Alderige <galbyoficial@gmail.com>
2025-06-06 11:52:08 -0300

    commit

--- script.js
+++ script.js
@@ -10,14 +10,50 @@ const button = document.getElementById("converterBtn")
 
 const API_URL = "https://api.exchangerate-api.com/v4/latest/"
 
-function convertMoney (){
+async function convertMoney() {
 
     loading.style.display = "block"
-    console.log("Funcionou")
+    error.style.display = "none"
+    result.style.display = "none"
+
+
+
+    //tentar
+    try {
+        const response = await fetch(API_URL + fromCurrency.value)
+        //JSON - javascript object notation
+        const data = await response.json()
+
+        const rate = data.rates[toCurrency.value]
+        const convertedValue = (amount.value * rate).toFixed(2)
+
+        convertedAmount.value = convertedValue
+        result.style.display = "block"
+
+        result.innerHTML = `
+        <div style="font-size 1.4rem;">
+            ${amount.value} ${fromCurrency.value} = ${convertedAmount.value} ${toCurrency.value}
+        </div>
+
+        <div style="font-size: 0.9rem; opacity: 0.8; margin-top: 10px;">
+            Taxa 1 ${fromCurrency.value} = ${rate} ${toCurrency.value}
+        </div>
+        
+        `
+
+
+    }
+    //deu erro
+    catch (err) {
+        error.style.display = "block"
+        error.innerHTML = `Falha ao converter moeda, tente novamente`
+    }
+
+    loading.style.display = "none"
 }
 
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", function (event) {
     event.preventDefault()
     convertMoney()
 })
