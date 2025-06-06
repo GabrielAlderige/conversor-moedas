22bb524f8a9dc6247425bbd23fb4bbcc95b9445c
Gabriel Alderige <galbyoficial@gmail.com>
2025-06-06 11:23:33 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("converterForm")
+const amount = document.getElementById("amount")
+const fromCurrency = document.getElementById("fromCurrency")
+const convertedAmount = document.getElementById("convertedAmount")
+const toCurrency = document.getElementById("toCurrency")
+const loading = document.querySelector(".loading")
+const result = document.querySelector(".result")
+const error = document.querySelector(".error")
+const button = document.getElementById("converterBtn")
+
+const API_URL = "https://api.exchangerate-api.com/v4/latest/"
+
+function convertMoney (){
+
+    loading.style.display = "block"
+    console.log("Funcionou")
+}
+
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+    convertMoney()
+})
